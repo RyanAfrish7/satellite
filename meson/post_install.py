b47084a6353f1ab70224cd0bae75b94fb513b744
Nathan Bonnemains <bonnemainsnathan@gmail.com>
2019-08-08 11:21:13 +0200

    Add post_install.py

---
+++ meson/post_install.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+
+schema_dir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
+icon_cache_dir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'icons', 'hicolor')
+
+if not os.environ.get('DESTDIR'):
+    print('Updating desktop icon cache…')
+    subprocess.call(['gtk-update-icon-cache', '-qtf', icon_cache_dir])
